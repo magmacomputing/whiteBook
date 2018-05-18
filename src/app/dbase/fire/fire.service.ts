612375530001ece15fb0eb912a00ea81e6d533f8
magmacomputing <michael@magmacomputing.com.au>
2018-05-19 09:00:19 +1000

    restart

--- src/app/dbase/fire/fire.service.ts
+++ src/app/dbase/fire/fire.service.ts
@@ -6,6 +6,7 @@ import { Observable, Subscription } from 'rxjs';
 import { tap } from 'rxjs/operators';
 
 import { DBaseModule } from '@app/dbase/dbase.module';
+import { isFunction } from '@lib/object.library';
 import { dbg } from '@lib/log.library';
 
 @Injectable({
@@ -21,20 +22,30 @@ export class FireService {
 
   private init() {
     this.dbg('init');
-    // this.snapOn('/client');
+    this.snapOn('/client', this.snapStore);
   }
 
   /** FireService will manage the sync of data from FireStore down to the client */
   public snapOn(collection: string, onNext?: (value: DocumentChangeAction<{}>[]) => void) {
     this.snapOff(collection);                 // turn off any prior Subscription
+
     this.listener[collection] = this.af.collection(collection)
       .snapshotChanges()
-      .subscribe(onNext)
+      .subscribe(snaps => this.dispatch(this.dbg, onNext, snaps))
   }
 
   public snapOff(collection: string) {
     if (this.listener[collection])
       this.listener[collection];
+  }
+
+  /** This is run as a callback */
+  private dispatch(dbg: Function, onNext: (value: DocumentChangeAction<{}>[]) => void, snaps: DocumentChangeAction<{}>[]) {
+    onNext(snaps);
+  }
+
+  private snapStore(snap: DocumentChangeAction<{}>[]) {
+    snap.forEach(chg => this.dbg('chg: %j', chg.type));
   }
 
 }
