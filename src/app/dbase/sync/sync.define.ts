6e16e59235edd7b27a24c0ee832ebc6de84b1f37
magmacomputing <michael@magmacomputing.com.au>
2018-08-01 21:07:58 +1000

    sync.define

--- src/app/dbase/sync/sync.define.ts
+++ src/app/dbase/sync/sync.define.ts
@@ -2,7 +2,7 @@ import { Subscription } from 'rxjs';
 import { IPromise } from '@lib/utility.interface';
 
 export interface IListen {
-	slice: string;												// slice of RX/store
+	slice: string;												// slice of ngxs/store
 	cnt: number;													// count of snapshots received
 	ready: IPromise<boolean>;							// indicate when snap0 is received
 	subscribe: Subscription;							// used for turning off RX subscription
