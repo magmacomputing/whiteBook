99f4f65156c21ce8d74073bc67b115beb3428e87
magmacomputing <michael@magmacomputing.com.au>
2018-08-21 18:16:07 +1000

    working version of profile$, cleaned {user, profile, price}

--- src/app/route/attend/attend.component.ts
+++ src/app/route/attend/attend.component.ts
@@ -44,19 +44,23 @@ export class AttendComponent implements OnInit {
   ngOnInit() {                                              // wire-up the Observables
     this.profile$ = this.auth$.pipe(
       map(auth => auth.userInfo),                           // get the current User UID
-      switchMap((user: UserInfo) => this.member$.pipe(                   // search the /member store for an effective ProfilePlan
+      map((user: UserInfo) => {
+        const { displayName, email, photoURL, providerId, uid } = user;
+        return { displayName, email, photoURL, providerId, uid }
+      }),
+      switchMap(user => this.member$.pipe(                   // search the /member store for an effective ProfilePlan
         map((state: IStoreState) => asAt<IProfilePlan>(state[STORE.profile] as IProfilePlan[],
           [{ fieldPath: FIELD.type, value: 'plan' }, { fieldPath: FIELD.key, value: user.uid }], this.date)),
-        map(table => Object.assign({}, { auth: user }, { profile: table[0] }),// return only the first occurrence
+        map(table => Object.assign({}, { user }, { profile: table[0] }),// return only the first occurrence
         )),
       ),
-      switchMap((result: any) => this.client$.pipe(
+      switchMap((result: any) => this.client$.pipe(         // get the prices[] for the effective ProfilePlan
         map((state: IStoreState) => asAt<IPrice>(state[STORE.price] as IPrice[],
           [{ fieldPath: FIELD.key, value: result.profile.plan }], this.date)),
         map(table => Object.assign({}, result, { price: table })),
       )),
     )
-    this.profile$.subscribe(plan => this.dbg('profile: %j', plan));
+    this.profile$.subscribe(profile => this.dbg('profile: %j', profile));
 
     this.default$ = this.client$.pipe(
       map((state: IStoreState) => state[STORE.default] as IDefault[]),
