feece3abee5195e5d76c8747a6d908126b8f42bf
Michael McRae <michael@magmacomputing.com.au>
2018-09-22 19:02:51 +1000

    tidy

--- src/app/route/profile/account/account.component.ts
+++ src/app/route/profile/account/account.component.ts
@@ -4,7 +4,6 @@ import { MemberService } from '@dbase/app/member.service';
 @Component({
   selector: 'wb-account',
   templateUrl: './account.component.html',
-  styleUrls: ['./account.component.css']
 })
 export class AccountComponent implements OnInit {
 
