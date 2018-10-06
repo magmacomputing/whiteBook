f3e06a5a8b797b0e2075e96b0d101f3aa4843d2b
Michael McRae <michael@magmacomputing.com.au>
2018-10-06 21:11:51 +1000

    add getDiff to date.library, default 'years'

--- src/app/library/date.library.ts
+++ src/app/library/date.library.ts
@@ -3,6 +3,7 @@ import * as moment from 'moment';
 import { DATE_FMT, MOMENT_FMT, IDate } from '@lib/date.define';
 import { TString } from '@lib/type.library';
 import { toNumeric } from '@lib/string.library';
+import { isNumber } from 'util';
 
 /** Helper functions to standardize Date/Time formats */
 
@@ -20,3 +21,8 @@ export const fmtDate = (dt?: any, fmt: TString = MOMENT_FMT): IDate => {
 
 export const getStamp = (dt?: any, fmt: TString = MOMENT_FMT) =>
   toNumeric(getMoment(dt, fmt).format(DATE_FMT.stamp)) as number;
+
+export const getDiff = (date: number, unit: moment.unitOfTime.Diff = 'years') =>
+  isNumber(date)
+    ? moment().diff(getMoment(date * 1000), unit)
+    : undefined
