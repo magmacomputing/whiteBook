cdfdef4bf7870580504be4afdb36f75ea67a2d5d
Michael McRae <mmcrae@mine.com.au>
2018-12-03 15:37:05 +1100

    purge unused object.library exports

--- src/app/library/object.library.ts
+++ src/app/library/object.library.ts
@@ -1,49 +1,9 @@
-import { isNumeric } from '@lib/string.library';
-import { isObject, getType, isArray, isString, TString, isNull } from '@lib/type.library';
-import { isUndefined } from 'util';
+import { getType, isObject, isArray, isString, TString, isNull, isUndefined } from '@lib/type.library';
 
 export interface IObject<T> { [key: string]: T; }
+const regex = /(?<matchWord>.*)\[(?<matchIdx>.)\]$/;// a pattern to find array-references
 
 /** Get nested value, allow for array-references in <path> */
-export const getPathX = <T>(obj: { [key: string]: any }, path: string, dflt?: any) => {
-	if (!isObject(obj))
-		return undefined;
-
-	let res = obj;
-	// let sub = obj;																	// reference to parent object
-
-	path.split('.')
-		.forEach(part => {
-			part = part.replace(' ', '');               // remove <spaces> (added for readability)
-
-			if (part.substring(part.length - 1) === ']') {
-				const ref = part.split('[');
-				const idx = ref[1].replace(']', '');
-
-				if (res && res[ref[0]]) {
-					res = idx === '*'
-						? res[ref[0]]                         // entire array
-						: res[ref[0]][idx]                    // else single element from array
-				}
-			} else {
-				// if (isArray(res)) {
-				// 	res = res.map((item, idx) => {
-				// 		let val = item[part];
-				// 		if (isUndefined(val) && dflt) {
-				// 			val = dflt;
-				// 			sub[idx] = dflt;
-				// 		}
-				// 		else res = res && res[part];
-				// 	})
-				// }
-				res = isArray(res)
-					? res.map(item => item[part])
-					: (res && res[part] || undefined)
-			}
-		})
-	return res;
-}
-
 export const getPath = (obj: any, path: TString, dflt?: any, indx?: string | number): any => {
 	if (!isObject(obj) && !isArray(obj))
 		return dflt || undefined;
@@ -54,7 +14,6 @@ export const getPath = (obj: any, path: TString, dflt?: any, indx?: string | num
 		? path.replace(' ', '').split('.')							// remove readability-spaces
 		: path
 
-	const regex = /(?<matchWord>.*)\[(?<matchIdx>.)\]$/;		// a pattern to find array-references
 	const match = regex.exec(word);										// eg. does the 'word' end in "*[0]"?
 	const { matchWord, matchIdx } = !isNull(match) && match.groups || { matchWord: word, matchIdx: '*' };
 
@@ -69,44 +28,6 @@ export const getPath = (obj: any, path: TString, dflt?: any, indx?: string | num
 		? getPath(obj, rest, dflt, matchIdx)						// recurse into object
 		: obj || dflt
 }
-
-/** Sort Object by its keys */
-export const sortObj = (obj: any, deep: boolean = true): any => {
-	const col: IObject<any> = {};
-
-	if (!isObject(obj))
-		return obj;
-
-	return Object.keys(obj)
-		.sort()
-		.reduce((col, key) => {
-			const type = getType(obj[key]);
-			if (deep && type !== 'Null' && type !== 'Undefined') {
-				switch (type) {
-					case 'Object':
-						col[key] = sortObj(obj[key], deep);   // recurse
-						break;
-
-					case 'Array':
-						col[key] = obj[key].every(isNumeric)
-							? obj[key].sort((a: number, b: number) => a - b)
-							: obj[key].sort()                   // sort Array    
-						break;
-
-					case 'Date':
-						col[key] = new Date();
-						col[key].setTime(obj[key].getTime());
-						break;
-
-					default:
-						col[key] = obj[key];
-						break;
-				}
-			}
-			else col[key] = obj[key];
-			return col;
-		}, col);
-};
 
 /** sort Object by multiple keys */
 export const sortKeys = (...keys: any[]): any => (a: any, b: any) => {
@@ -123,44 +44,6 @@ export const sortKeys = (...keys: any[]): any => (a: any, b: any) => {
 	}
 }
 
-/** lowerCase Object keys */
-export const lowerObj = (obj: any) => {
-	if (!isObject(obj))
-		return obj;
-
-	let newObj: IObject<any>;
-	switch (getType(obj)) {
-		case 'Object':
-			newObj = {};
-			Object.keys(obj).forEach(key => newObj[key.toLowerCase()] = lowerObj(obj[key]));
-			break;
-
-		case 'Array':
-			newObj = obj.map((key: any, idx: number) => lowerObj(obj[idx]));
-			break;
-
-		default:
-			newObj = obj;
-			break;
-	}
-
-	return newObj;
-}
-
-/** Convert Array[{}] to Object{{}} */
-export const objArray = (obj: any) => {
-	if (isArray(obj) && isObject(obj[0])) {
-		const newObj: IObject<any> = {};
-
-		obj.forEach(itm =>
-			Object.keys(itm)
-				.forEach(key => newObj[key] = itm[key])
-		)
-		return newObj;
-	}
-	else return obj;
-}
-
 /** deep-clone Object */
 export const cloneObj = <T>(obj: T): T => {
 	try {
@@ -171,15 +54,14 @@ export const cloneObj = <T>(obj: T): T => {
 };
 
 export const deepClone = (obj: any): any => {
-	const type = getType(obj);
-
 	if (isArray(obj))
 		return obj.map(deepClone);
 
 	if (!isObject(obj))
 		return obj;
 
-	return Object.keys(obj).map(itm => deepClone(obj[itm]));
+	return Object.keys(obj)
+		.map(itm => deepClone(obj[itm]));
 }
 
 /** deep-compare Objects for equality */
