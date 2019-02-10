f9db1c2d683eaf15684b43e9b6a39645c7367a8a
Michael McRae <michael@magmacomputing.com.au>
2019-02-10 18:58:57 +1100

    proto googleapis

--- src/app/route/migrate/attend/migAttend.component.ts
+++ src/app/route/migrate/attend/migAttend.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { google, sheets_v4 } from 'googleapis';
+
 @Component({
 	selector: 'wb-migrate',
 	templateUrl: './migAttend.component.html',
@@ -8,6 +10,15 @@ export class MigAttendComponent implements OnInit {
 
 	constructor() { }
 
-	ngOnInit() { }
+	ngOnInit() {
+		const sheets = google.sheets('v4');
+		const request = {//: sheets_v4.Params$Resource$Spreadsheets$Get = {
+			spreadsheetId: '16hTR03kU6aZY1am2EtQ2iujllnwIQ7lURE2bHpcXhhw',
+			ranges: ['A1:F13'],
+		};
+
+		sheets.spreadsheets.get(request)
+			.then((response: any) => console.log('response'));
+	}
 
 }
