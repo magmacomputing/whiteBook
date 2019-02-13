a17acc4c77620780bb1da3bd4a69dba3b1ee2f81
Michael McRae <michael@magmacomputing.com.au>
2019-02-13 18:46:26 +1100

    merge

--- src/app/route/migrate/attend/migAttend.component.ts
+++ src/app/route/migrate/attend/migAttend.component.ts
@@ -2,23 +2,26 @@ import { Component, OnInit } from '@angular/core';
 
 import { google, sheets_v4 } from 'googleapis';
 
+import {} from '@lib/logger.library';
+
 @Component({
 	selector: 'wb-migrate',
 	templateUrl: './migAttend.component.html',
 })
 export class MigAttendComponent implements OnInit {
+	private dbg: 
 
 	constructor() { }
 
 	ngOnInit() {
 		const sheets = google.sheets('v4');
-		const request = {//: sheets_v4.Params$Resource$Spreadsheets$Get = {
+		const request: sheets_v4.Params$Resource$Spreadsheets$Get = {
 			spreadsheetId: '16hTR03kU6aZY1am2EtQ2iujllnwIQ7lURE2bHpcXhhw',
 			ranges: ['A1:F13'],
 		};
 
 		sheets.spreadsheets.get(request)
-			.then((response: any) => console.log('response'));
+			.then(response=> console.log('response: '));
 	}
 
 }
