367f207aa39960ac495648484ebaeecb6727111d
Michael McRae <michael@magmacomputing.com.au>
2019-02-16 09:08:16 +1100

    @angular@7.2.5

--- src/app/route/migrate/attend/migAttend.component.ts
+++ src/app/route/migrate/attend/migAttend.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-// import { google, sheets_v4 } from 'googleapis';
+import { google, sheets_v4 } from 'googleapis';
 
 import { dbg } from '@lib/logger.library';
 
@@ -14,14 +14,14 @@ export class MigAttendComponent implements OnInit {
 	constructor() { }
 
 	ngOnInit() {
-		// const sheets = google.sheets('v4');
-		// const request: sheets_v4.Params$Resource$Spreadsheets$Get = {
-		// 	spreadsheetId: '16hTR03kU6aZY1am2EtQ2iujllnwIQ7lURE2bHpcXhhw',
-		// 	ranges: ['A1:F13'],
-		// };
+		const sheets = google.sheets('v4');
+		const request: sheets_v4.Params$Resource$Spreadsheets$Get = {
+			spreadsheetId: '16hTR03kU6aZY1am2EtQ2iujllnwIQ7lURE2bHpcXhhw',
+			ranges: ['A1:F13'],
+		};
 
-		// sheets.spreadsheets.get(request)
-		// 	.then(response => console.log('response: ', response));
+		sheets.spreadsheets.get(request)
+			.then(response => console.log('response: ', response));
 	}
 
 }
