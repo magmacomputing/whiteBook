7b0e0e7b7153e1708ee930f79679e55d8023fad2
Michael McRae <mmcrae@mine.com.au>
2019-02-18 16:14:59 +1100

    working...

--- src/app/route/migrate/attend/mig-attend.component.ts
+++ src/app/route/migrate/attend/mig-attend.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
+import { dbg } from '@lib/logger.library';
+
 @Component({
   selector: 'wb-mig-attend',
   templateUrl: './mig-attend.component.html',
 })
 export class MigAttendComponent implements OnInit {
-
+  private dbg = dbg(this);
+  
   constructor() { console.log('migAttend'); }
 
   ngOnInit() { }
