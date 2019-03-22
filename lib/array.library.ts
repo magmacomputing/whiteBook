5bd27e3ea119f163b9903b55bdbb7513758f8c80
Michael McRae <mmcrae@mine.com.au>
2019-03-22 14:58:06 +1100

    cartesian

--- lib/array.library.ts
+++ lib/array.library.ts
@@ -4,3 +4,7 @@ export const asArray = <T>(arr: T | T[] = []) => isArray(arr) ? [...arr] : [arr]
 
 // useful for de-duping an array of values
 export const deDup = <T>(...value: T[]) => [... new Set(value)];
+
+// useful for cartesian product of arrays
+const cartFn = (a: any, b: any) => [].concat(...a.map((d: any) => b.map((e: any) => [].concat(d, e))));
+export const cartesian: any = (a: any, b: any, ...c: any) => (b ? cartesian(cartFn(a, b), ...c) : a);
--- src/app/dbase/fire/fire.library.ts
+++ src/app/dbase/fire/fire.library.ts
@@ -2,9 +2,9 @@ import * as firebase from 'firebase/app';
 import { Query, FieldPath, QueryFn } from '@angular/fire/firestore';
 import { IQuery, IWhere } from '@dbase/fire/fire.interface';
 
-import { asArray } from '@lib/array.library';
+import { asArray, cartesian } from '@lib/array.library';
 import { isNumeric } from '@lib/string.library';
-import { isUndefined, isArray } from '@lib/type.library';
+import { isUndefined } from '@lib/type.library';
 
 /** Array of Query Functions with any limit / order criteria.  
  * If a Where-clause contains an array of <value>, this indicates a logical-or,
@@ -39,15 +39,18 @@ export const fnQuery = (query: IQuery = {}) => {
 /** create a set of Where-clauses to split the logical-or criteria */
 const splitQuery = (query: IQuery) => {
 	const split: IQuery[] = [];
-	const check: [number, number][] = [];
+	const wheres = asArray(query.where);
 
 	if (query.where) {
-		asArray(query.where).forEach((where, w) =>
-			asArray(where.value).forEach((_value, v, arr) => {
-				if (arr.length > 1)
-					check.push([w, v])
-			})
-		)
+		const vals = wheres.map(where => asArray(where.value).map((_value, v) => v));
+		const rows: number[][] = cartesian(...vals);
+
+		rows.forEach(row => {
+			split.push({
+				limitTo: query.limitTo, orderBy: query.orderBy,
+				where: row.map((itm, idx) => ({ fieldPath: wheres[idx].fieldPath, opStr: wheres[idx].opStr, value: wheres[idx].value[itm] }))
+			});
+		})
 	}
 	return split.length ? split : [query];
 }
