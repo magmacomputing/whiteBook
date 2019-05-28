f54b5c5d231b58b14556647148f293a483aa80c9
Michael McRae <michael@magmacomputing.com.au>
2019-05-28 18:09:07 +1000

    put distinct back on Array.prototype

--- lib/array.library.ts
+++ lib/array.library.ts
@@ -8,35 +8,20 @@ export const cartesian: any = (a: any[], b?: any[], ...c: any[]) => (b ? cartesi
 
 
 declare global {
-	interface ArrayExt<T> {												// change the global interface for Array
+	interface Array<T> {												// change the global interface for Array
 		/** reduce an Array to remove duplicate values */
-		distinct<U>(callbackfn?: (value: T) => U): T extends U ? ArrayExt<T> : ArrayExt<U>;
-		
-		cartesian: () => T[][];
+		distinct<U>(callbackfn?: (value: T) => U): T extends U ? Array<T> : Array<U>;
+
+		cartesian: () => T[][];										// TODO
 	}
 }
-export class ArrayExt<T> extends Array {
-	constructor(...args: any[]) {
-		super(...args);
-	}
-	
-	// useful for de-duping an array of values
-	distinct = function <U>(selector?: <T>(value: T) => U) {
+
+if (!Array.prototype.hasOwnProperty('distinct'))
+	Array.prototype.distinct = function <U>(selector?: <T>(value: T) => U) {
 		return selector
 			? this.map(selector).distinct()
 			: [...new Set(this)]
 	}
-
-	cartesian = function () {
-
-	}
-}
-// if (!Array.prototype.hasOwnProperty('distinct'))
-// 	Array.prototype.distinct = function <U>(selector?: <T>(value: T) => U) {
-// 		return selector
-// 			? this.map(selector).distinct()
-// 			: [...new Set(this)]
-// 	}
 
 // if (!Array.prototype.hasOwnProperty('cartesian'))
 // 	Array.prototype.cartesian = (a: any[], b?: any[], ...c: any[]) => (b ? cartesian(cartFn(a, b), ...c) : a);
