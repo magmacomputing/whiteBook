926dac2c86dca42506920cbec3652b35dbe5067f
Michael McRae <michael@magmacomputing.com.au>
2019-06-01 11:03:44 +1000

    experiment with AngularFireAuthGuard

--- src/app/route/router/route.module.ts
+++ src/app/route/router/route.module.ts
@@ -2,26 +2,32 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
+import { AngularFireAuthGuardModule, AngularFireAuthGuard, customClaims, canActivate } from '@angular/fire/auth-guard';
+
+import { pipe } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 import { ROUTE } from '@route/route.define';
-import { AuthGuard, ProfileGuard, DeactivateGuard } from '@route/route.guard';
+import { ProfileGuard, DeactivateGuard } from '@route/route.guard';
 import { MaterialModule } from '@route/material.module';
 import { LoginComponent } from '@route/login/login.component';
 import { AttendComponent } from '@route/attend/attend.component';
 import { OAuthComponent } from '@route/login/oauth.component';
 
+const admin = pipe(customClaims, map(claims => claims.roles && claims.roles.includes('admin')));
+
 const routes: Routes = [
 	{ path: ROUTE.oauth, component: OAuthComponent, canDeactivate: [DeactivateGuard] },				// todo: cannot be lazy-loaded
 	{ path: ROUTE.login, component: LoginComponent },
-	{ path: ROUTE.attend, component: AttendComponent, canActivate: [AuthGuard, ProfileGuard] },
-	{ path: ROUTE.profile, loadChildren: () => import('@route/profile/profile.module').then(m => m.ProfileModule), canActivate: [AuthGuard] },
-	{ path: ROUTE.admin, loadChildren: () => import('@route/admin/admin.module').then(m => m.AdminModule), canActivate: [AuthGuard] },
-	{ path: ROUTE.migrate, loadChildren: () => import('@route/migrate/migrate.module').then(m => m.MigrateModule), canActivate: [AuthGuard] },
+	{ path: ROUTE.attend, component: AttendComponent, canActivate: [AngularFireAuthGuard, ProfileGuard] },
+	{ path: ROUTE.profile, loadChildren: () => import('@route/profile/profile.module').then(m => m.ProfileModule), canActivate: [AngularFireAuthGuard] },
+	{ path: ROUTE.admin, loadChildren: () => import('@route/admin/admin.module').then(m => m.AdminModule), ...canActivate(admin) },
+	{ path: ROUTE.migrate, loadChildren: () => import('@route/migrate/migrate.module').then(m => m.MigrateModule), ...canActivate(admin) },
 	{ path: '**', redirectTo: '/attend', pathMatch: 'full' },
 ];
 
 @NgModule({
-	imports: [CommonModule, MaterialModule, HttpClientModule, RouterModule.forRoot(routes),],
+	imports: [CommonModule, MaterialModule, HttpClientModule, AngularFireAuthGuardModule, RouterModule.forRoot(routes),],
 	exports: [RouterModule],
 	declarations: [LoginComponent, AttendComponent, OAuthComponent],
 })
