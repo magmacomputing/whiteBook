164634eef24435065ddcf3304cb8e52677379c40
Michael McRae <michael@magmacomputing.com.au>
2019-06-04 17:04:24 +1000

    ensure unsubscribe

--- src/app/route/login/oauth.component.ts
+++ src/app/route/login/oauth.component.ts
@@ -31,6 +31,7 @@ export class OAuthComponent implements OnInit {
 
 					this.http.post<any>(url, {})
 						.subscribe(res => this.auth.signInToken(res))
+						.unsubscribe()
 				})
 				.catch(err => this.dbg('error: %s', err.message))
 		}
