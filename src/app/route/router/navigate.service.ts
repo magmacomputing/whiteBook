0dde34627bd152c6e6602dab25fdaf8d698c5f27
Michael McRae <mmcrae@mine.com.au>
2019-06-12 15:59:04 +1000

    remove debugs

--- src/app/route/router/navigate.service.ts
+++ src/app/route/router/navigate.service.ts
@@ -1,31 +1,16 @@
 import { Injectable, NgZone } from '@angular/core';
-import { Router, UrlTree, NavigationEnd } from '@angular/router';
-import { filter } from 'rxjs/operators';
+import { Router, UrlTree } from '@angular/router';
 
 import { dbg } from '@lib/logger.library';
 
 @Injectable({ providedIn: 'root' })
 export class NavigateService {
   private dbg = dbg(this);
-  // private prevUrl: string | null = null;
-  // private currUrl: string | null = null;
 
-  constructor(private router: Router, private zone: NgZone) {
-    this.dbg('new');
-    // this.currUrl = this.router.url;
-    // this.router.events
-    //   .pipe(filter(event => event instanceof NavigationEnd))
-    //   .subscribe(event => {
-    //     if (event instanceof NavigationEnd) {
-    //       this.prevUrl = this.currUrl;
-    //       this.currUrl = event.url;
-    //     }
-    //   })
-  }
+  constructor(private router: Router, private zone: NgZone) { this.dbg('new'); }
 
   route(url: string | UrlTree) {
     this.dbg('route: /%s', url);
-    // this.dbg('urls: %j', this.url);
 
     return new Promise<boolean>((resolve, reject) =>
       this.zone.run(_ =>
@@ -39,7 +24,4 @@ export class NavigateService {
   get url() {
     return this.router.url;
   }
-  // get urls(): [string | null, string | null] {
-  //   return [this.prevUrl, this.currUrl];
-  // }
 }
