7d81d85efd9878d06359373de0f50ec31970182e
Michael McRae <michael@magmacomputing.com.au>
2019-08-24 16:50:58 +1000

    remove debug from attendService

--- src/app/service/member/attend.service.ts
+++ src/app/service/member/attend.service.ts
@@ -59,7 +59,6 @@ export class AttendService {
 
 	/** Insert an Attend document, aligned to an active Payment  */
 	public setAttend = async (schedule: ISchedule, note?: TString, date?: TDate) => {
-		debugger;
 		const creates: IStoreMeta[] = [];
 		const updates: IStoreMeta[] = [];
 
