abbc1c1a8868faffd633c3ab9d72ff5131530d93
Michael McRae <michael@magmacomputing.com.au>
2020-03-21 12:10:08 +1100

    remove dbg class.library

--- lib/class.library.ts
+++ lib/class.library.ts
@@ -1,5 +1,3 @@
-import { dbg } from '@lib/logger.library';
-
 // Typescript Decorators
 
 export function Sealed(constructor: Function) {
@@ -9,7 +7,6 @@ export function Sealed(constructor: Function) {
 
 export function Debug<T extends { new(...args: any[]): {} }>(constructor: T) {
 	return class extends constructor {
-		private dbg = dbg(this);
 		constructor(...args: any[]) {
 			super(...args);
 		}
