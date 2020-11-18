13d0fb458360dc6694ebb8ac485c14634d482f91
Michael McRae <michael@magmacomputing.com.au>
2020-11-19 10:53:29 +1100

    WorkerService

--- src/app/service/worker/worker.service.ts
+++ src/app/service/worker/worker.service.ts
@@ -1,21 +1,38 @@
 import { Injectable } from '@angular/core';
 
-import { isDefined } from '@library/type.library';
+import { isUndefined } from '@library/type.library';
+import { dbg } from '@library/logger.library';
 
+// TODO:  Can this be made multi-instance and still be Injectable
 @Injectable({ providedIn: 'root' })
 export class WorkerService {
+	#dbg = dbg(this, 'WorkerService');
+	#worker!: Worker;
 
 	constructor() {
-		if (isDefined(Worker)) {
-			// Create a new
-			const worker = new Worker('./app.worker', { type: 'module' });
-			worker.onmessage = ({ data }) => {
-				console.log(`page got message: ${data}`);
-			};
-			worker.postMessage('hello');
-		} else {
-			// Web Workers are not supported in this environment.
-			// You should add a fallback so that your program still executes correctly.
+		const script = './app.worker';
+		this.#dbg('new: %s', script);
+
+		if (isUndefined(Worker)) {
+			this.#dbg('Web Workers are not supported in this environment');
+			return;
 		}
+
+		this.#worker = new Worker('./app.worker', { type: 'module' });	// Start a Worker
+		this.#worker.onmessage = this.recv.bind(this);					// Define a handler
+
+		this.send(`init: ${script}`);
+	}
+
+	start(worker: string | URL) {
+
+	}
+
+	send(msg: any) {
+		this.#worker?.postMessage(msg);
+	}
+
+	private recv({ data }: any) {
+		this.#dbg(`page got message: ${data}`);
 	}
 }
