9fd6e3323f6994c684fa238d660f752875449998
Michael McRae <michael@magmacomputing.com.au>
2021-02-24 14:26:58 +1100

    instant

--- core/library/instant.library.ts
+++ core/library/instant.library.ts
@@ -453,7 +453,7 @@ export namespace Instant {
 	export type TYPE = string | number | Date | Instant | null;
 
 	export enum WEEKDAY { All, Mon, Tue, Wed, Thu, Fri, Sat, Sun };
-	export enum WEEKDAYS { Every, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday };
+	export enum WEEKDAYS { Everyday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday };
 	export enum MONTH { All, Jan, Feb, Mar, Apr, May, Jun, Jul, Aug, Sep, Oct, Nov, Dec };
 	export enum MONTHS { Every, January, February, March, April, May, June, July, August, September, October, November, December };
 	export enum DURATION { year, month, week, day, hour, minute, second };
